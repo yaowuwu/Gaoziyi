7752ddcf100b95225965f2470fc6b810289672b5
1239012237 <1239012237@qq.com>
2020-03-10 22:42:38 +0800

    添加了 视图函数

--- user/apis.py
+++ user/apis.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+
+def get_vcode(request):
+    '''获取短信验证码'''
+    return JsonResponse({})
+
+
+def submit_vcode(request):
+    '''通过验证码登陆，注册'''
+    return JsonResponse({})
+
+
+def show_profile(request):
+    '''查看个人交友资料'''
+    return JsonResponse({})
+
+
+def modify_profile(request):
+    '''修改个人资料及交友资料'''
+    return JsonResponse({})
+
+
+def upload_avatar(request):
+    '''头像上传'''
+    return JsonResponse({})
--- user/models.py
+++ user/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    GENDER = (
+        ('male','男性'),
+        ('female','女性'),
+    )
+    LOCATION = (
+        ('北京', '北京'),
+        ('上海', '上海'),
+        ('深圳', '深圳'),
+        ('郑州', '郑州'),
+        ('西安', '西安'),
+        ('武汉', '武汉'),
+        ('成都', '成都'),
+        ('沈阳', '沈阳'),
+
+
+    )
+    phonenum = models.CharField(max_length=15, verbose_name='手机号')
+    nickname = models.CharField(max_length=32, verbose_name='昵称')
+    gender = models.CharField(max_length=10, choices=GENDER, default='male', verbose_name='性别')
+    birthday = models.DateField(default='2000-01-01', verbose_name='生日')
+    avatar = models.CharField(max_length=256, verbose_name='个人形象的URL')
+    location = models.CharField(max_length=10, verbose_name='常居地', choices=LOCATION)
